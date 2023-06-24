4b0d5059ec3b506ec64e0f0bd67081a02d0bb87f
sergiuplesco <plescosergiu@gmail.com>
2023-06-24 21:19:17 +0300

    used env to access env variables

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -48,19 +48,9 @@ export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId:
-        "568765637415-0rjmm0nkd2jlc47vl323k65jh89760q3.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
-    /**
-     * ...add more providers here.
-     *
-     * Most other providers require a bit more work than the Discord provider. For example, the
-     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
-     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
-     *
-     * @see https://next-auth.js.org/providers/github
-     */
   ],
   secret: "gasdt54yadsgjl4334tq4tjvj",
 };
